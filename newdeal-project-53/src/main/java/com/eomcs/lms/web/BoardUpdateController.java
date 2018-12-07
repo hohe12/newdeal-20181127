60eaaf6dfe46e351385381217b44279f9d590eb5
송기원 <song-giwon@song-giwon-ui-MacBook-Pro.local>
2018-12-07 17:51:00 +0900

    v56

---
+++ newdeal-project-53/src/main/java/com/eomcs/lms/web/BoardUpdateController.java
@@ -0,0 +1,34 @@
+package com.eomcs.lms.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.eomcs.lms.dao.BoardDao;
+import com.eomcs.lms.domain.Board;
+
+@Component("/board/update")
+public class BoardUpdateController implements PageController {
+
+
+  BoardDao boardDao;
+  
+  public BoardUpdateController(BoardDao boardDao) {
+	    this.boardDao = boardDao;
+	  }
+@Override
+public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    Board board = new Board();
+    board.setNo(Integer.parseInt(request.getParameter("no")));
+    board.setContents(request.getParameter("contents"));
+    
+    boardDao.update(board);
+    
+	return "redirect:list";
+}
+}
